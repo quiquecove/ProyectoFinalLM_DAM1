83614a055dd7478b0bca31990fd2b6ff4c36d8da
Enrique Huerta Lopez <quiquehuertalopez@gmail.com>
2023-05-20 20:50:17 +0200

    web con nav sliders, barra lateral

---
+++ main.js
@@ -0,0 +1,37 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+  });
+
+  var swiper = new Swiper(".coming-container",{
+spaceBetween:20,
+loop:true,
+autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  centeredSlides:true,
+  breakpoints:{
+    0:{
+        slidesPerView:2,
+    },
+    568:{
+        slidesPerView:3,
+    },
+    768:{
+        slidesPerView:4,
+    },
+    968:{
+        slidesPerView:5,
+    },
+  },
+  });
